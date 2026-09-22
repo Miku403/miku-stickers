387f88d8b4ca37ee940f331dbe68bc298cf0a181
Miku403 <ayhamgumari@gmail.com>
2026-09-22 14:31:36 +0300

    Update WhatsAppBridge.swift

--- MikuStickersApp/WhatsAppBridge.swift
+++ MikuStickersApp/WhatsAppBridge.swift
@@ -1,1 +1,65 @@
+import Foundation
+import UIKit
 
+final class WhatsAppBridge {
+
+    static let shared = WhatsAppBridge()
+
+    private init() {}
+
+    func importPack(
+        identifier: String,
+        name: String,
+        publisher: String,
+        trayImageBase64: String,
+        stickers: [[String: Any]]
+    ) throws {
+
+        let pack: [String: Any] = [
+            "ios_app_store_link": "",
+            "android_play_store_link": "",
+            "identifier": identifier,
+            "name": name,
+            "publisher": publisher,
+            "tray_image": trayImageBase64,
+            "stickers": stickers
+        ]
+
+        guard JSONSerialization.isValidJSONObject(pack) else {
+            throw NSError(
+                domain: "MikuStickers",
+                code: 1,
+                userInfo: [
+                    NSLocalizedDescriptionKey:
+                        "بيانات الحزمة غير صالحة"
+                ]
+            )
+        }
+
+        let jsonData = try JSONSerialization.data(
+            withJSONObject: pack,
+            options: []
+        )
+
+        UIPasteboard.general.setItems(
+            [[
+                "net.whatsapp.third-party.sticker-pack": jsonData
+            ]],
+            options: [:]
+        )
+
+        guard let url = URL(
+            string: "whatsapp://stickerPack"
+        ) else {
+            return
+        }
+
+        DispatchQueue.main.async {
+            UIApplication.shared.open(
+                url,
+                options: [:],
+                completionHandler: nil
+            )
+        }
+    }
+}
