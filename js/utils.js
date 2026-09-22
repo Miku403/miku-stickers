da6e55dfe485831ad935b827b53308a9f343d6f1
Miku403 <ayhamgumari@gmail.com>
2026-09-22 13:45:37 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,34 @@
+export function blobToBase64(blob) {
 
+  return new Promise((resolve, reject) => {
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+
+      const result = reader.result;
+
+      resolve(
+        result.split(",")[1]
+      );
+    };
+
+    reader.onerror = reject;
+
+    reader.readAsDataURL(blob);
+  });
+}
+
+
+export function formatBytes(bytes) {
+
+  if (bytes < 1024) {
+    return `${bytes} B`;
+  }
+
+  if (bytes < 1024 * 1024) {
+    return `${(bytes / 1024).toFixed(1)} KB`;
+  }
+
+  return `${(bytes / 1024 / 1024).toFixed(2)} MB`;
+}
