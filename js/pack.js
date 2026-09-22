4675f9b9a7af3fd564590badffc4575b0e95af45
Miku403 <ayhamgumari@gmail.com>
2026-09-22 13:46:36 +0300

    Update pack.js

--- js/pack.js
+++ js/pack.js
@@ -1,1 +1,64 @@
+import {
+  blobToBase64
+} from "./utils.js";
 
+
+export async function createPack(
+  packName,
+  stickers
+) {
+
+  if (
+    stickers.length < 3 ||
+    stickers.length > 30
+  ) {
+
+    throw new Error(
+      "يجب اختيار من 3 إلى 30 ملصقًا"
+    );
+
+  }
+
+
+  const trayImage =
+    await blobToBase64(
+      stickers[0].blob
+    );
+
+
+  return {
+
+    ios_app_store_link: "",
+    android_play_store_link: "",
+
+    identifier:
+      "miku_stickers_" +
+      Date.now(),
+
+    name:
+      packName.trim() ||
+      "Miku Stickers",
+
+    publisher:
+      "Miku Stickers",
+
+    tray_image:
+      trayImage,
+
+    stickers:
+      stickers.map(
+        (sticker, index) => ({
+
+          image_data:
+            sticker.base64,
+
+          emojis: ["😀"],
+
+          accessibility_text:
+            `Miku Sticker ${index + 1}`
+
+        })
+      )
+
+  };
+}
