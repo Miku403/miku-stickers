997aa117624543b998f957c6a0d69356e1375052
Miku403 <ayhamgumari@gmail.com>
2026-09-22 14:32:09 +0300

    Update ContentView.swift

--- MikuStickersApp/ContentView.swift
+++ MikuStickersApp/ContentView.swift
@@ -1,1 +1,41 @@
+import SwiftUI
+import WebKit
 
+struct ContentView: View {
+
+    var body: some View {
+        MikuWebView()
+            .ignoresSafeArea()
+    }
+}
+
+struct MikuWebView: UIViewRepresentable {
+
+    let url = URL(
+        string: "https://miku-stickers.vercel.app/"
+    )!
+
+    func makeUIView(context: Context) -> WKWebView {
+
+        let configuration = WKWebViewConfiguration()
+
+        let webView = WKWebView(
+            frame: .zero,
+            configuration: configuration
+        )
+
+        webView.allowsBackForwardNavigationGestures = true
+
+        webView.load(
+            URLRequest(url: url)
+        )
+
+        return webView
+    }
+
+    func updateUIView(
+        _ webView: WKWebView,
+        context: Context
+    ) {}
+
+}
